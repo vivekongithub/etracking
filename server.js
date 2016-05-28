d17ab7c8cfb8e20ed07dea878ce16d58e7620e94
Vivek Tyagi <vivekcse.2005@gmail.com>
2016-05-28 21:26:10 +0530

    added /api/authenticate route

--- server.js
+++ server.js
@@ -35,8 +35,43 @@ app.use(morgan('dev'));
 // get an instance of the router for api routes
 var apiRoutes = express.Router();
 
-// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
 
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: '24h' // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
 // TODO: route middleware to verify a token
 
 // route to show a random message (GET http://localhost:8080/api/)
@@ -50,6 +85,7 @@ apiRoutes.get('/users', function(req, res) {
         res.json(users);
     });
 });
+
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
