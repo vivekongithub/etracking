97291740befdfb879833cad73bfaa4bc0d191758
Vivek Tyagi <vivekcse.2005@gmail.com>
2016-05-31 11:11:08 +0530

    added routing middleware

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret:'nodemadeitsupersimple',
     database:'mongodb://admin:admin@ds019053.mlab.com:19053/etracking'
+    //database:'mongodb://admin:admin@127.0.0.1:27017/etracking'
 }
