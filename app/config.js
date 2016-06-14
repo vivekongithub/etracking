6857aae333970f55ba92072e743c2f5fa0c13dd2
Vivek Tyagi <vivekcse.2005@gmail.com>
2016-06-14 15:25:16 +0530

    removed config

--- app/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret:'nodemadeitsupersimple',
-    database:'mongodb://admin:admin@ds019053.mlab.com:19053/etracking'
-    //database:'mongodb://admin:admin@127.0.0.1:27017/etracking'
-}
